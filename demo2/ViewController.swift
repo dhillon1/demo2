70f39e2911711c789ddd14dffac3473d07a611bc
Simran Dhillon <simrandhillon5486@gmail.com>
2019-09-23 13:12:46 -0400

    bcdldnd

--- demo2/ViewController.swift
+++ demo2/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        newlocale(<#T##Int32#>, <#T##UnsafePointer<Int8>!#>, <#T##locale_t!#>)
     }
 
 
