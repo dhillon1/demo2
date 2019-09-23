b0a18dc3274cd76dfd1a41897b71c2dd1ecf241e
Simran Dhillon <simrandhillon5486@gmail.com>
2019-09-23 13:11:46 -0400

    Initial Commit

---
+++ demo2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  demo2
+//
+//  Created by Simran on 2019-09-23.
+//  Copyright © 2019 iosdemo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
