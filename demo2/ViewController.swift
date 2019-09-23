2e92428738b0ba00e45faada150711678277c6c7
Simran Dhillon <simrandhillon5486@gmail.com>
2019-09-23 13:19:15 -0400

    clv n c f lfmek

--- demo2/ViewController.swift
+++ demo2/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         newlocale(<#T##Int32#>, <#T##UnsafePointer<Int8>!#>, <#T##locale_t!#>)
+        
     }
 
 
